991701b3f25d2571baaecd9d0081c94ddb285f27
chris_l <cligare@hotmail.com>
2022-06-15 14:57:57 -0700

    JS and CSS code added if no rating selected

--- js/script.js
+++ js/script.js
@@ -5,16 +5,22 @@ const numbers = document.getElementById("rating-scale-container");
 const submitButton = document.getElementById("submit-btn");
 const thankYouScreen = document.getElementById("thank-you-state");
 const selectedRating = document.getElementById("selected-rating");
-
-submitButton.addEventListener("click", function () {
-  submit();
-});
+const errorMsg = document.getElementById("error-message");
 
 numbers.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
-    let chosenBtn = e.target.textContent;
+    let chosenBtn = e.target.innerHTML;
     selectedRating.textContent = chosenBtn;
+  }
+});
+
+submitButton.addEventListener("click", (e) => {
+  if (selectedRating.textContent > 0) {
     ratingScreen.classList.add("hide");
     thankYouScreen.classList.remove("hide");
+  } else {
+    submitButton.classList.add("shake-horizontal");
+    errorMsg.textContent = `Please select your rating`;
+    errorMsg.classList.add("fade-in-fwd");
   }
 });
